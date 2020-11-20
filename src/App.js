9d0d626f76c575455da4fa52479107b8714fffc4
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-20 08:27:57 -0800

    using react router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import GlobalStyle from './components/GlobalStyle'
 //import pages
 import AboutUs from './pages/AboutUs';
 import AboutSection from './components/AboutSection';
-import Nav from './components/Nav'
+import ContactUs from './pages/ContactUs';
+import OurWork from './pages/OurWork';
+import Nav from './components/Nav';
+// router
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
