c43ab22d405d094f798b710a2eb5553aebf04189
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-20 10:28:32 -0800

    added path to bring us to a url that is a 'child' of /work

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AboutSection from './components/AboutSection';
 import ContactUs from './pages/ContactUs';
 import OurWork from './pages/OurWork';
 import Nav from './components/Nav';
+import MovieDetail from './pages/MovieDetail';
 // router
 import { Switch, Route } from 'react-router-dom';
 
@@ -19,8 +20,11 @@ function App() {
         <Route path="/" exact>
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route path="/work" exact>
           <OurWork />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetail />
         </Route>
         <Route path="/contact">
           <ContactUs />
