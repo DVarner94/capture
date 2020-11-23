50a1512a6178218a5381491c37f3f8241dd3ef55
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-23 10:30:41 -0800

    staggering animation on about section

--- src/animation.js
+++ src/animation.js
@@ -1,21 +1,29 @@
 export const pageAnimation = {
-    hidden: {
-      opacity: 0,
-      y: 300,
+  hidden: {
+    opacity: 0,
+    y: 300,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      when: 'beforeChildren',
+      staggerChildren: 0.25,
     },
-    show: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.5,
-        when: "beforeChildren",
-        staggerChildren: 0.25,
-      },
+  },
+  exit: {
+    opacity: 0,
+    transition: {
+      duration: 0.5,
     },
-    exit: {
-      opacity: 0,
-      transition: {
-        duration: 0.5,
-      },
-    },
-  };
+  },
+};
+
+export const titleAnimation = {
+  hidden: { y: 200 },
+  show: {
+    y: 0,
+    transition: { duration: .75, 'easeOut'}
+  },
+};
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -3,6 +3,7 @@ import home1 from '../img/home1.png';
 // styled
 import { About, Description, Hide, Image } from '../styles';
 import { motion } from 'framer-motion';
+import { titleAnim } from '../animation'
 
 const AboutSection = () => {
 
@@ -11,15 +12,15 @@ const AboutSection = () => {
             <Description>
                 <motion.div className="title">
                     <Hide>
-                        <motion.h2>We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             you <span>dreams</span> come
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>true.</motion.h2>
+                        <motion.h2 variants={titleAnim}>true.</motion.h2>
                     </Hide>
                 </motion.div>
                 <p>Contact us for any multimedia services you may need!</p>
