6e97f7373ac5a40dc90b5574105659325311bcb3
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-24 19:51:17 -0800

    implement scroll animations

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -7,11 +7,15 @@ import goodtimes from '../img/goodtimes-small.png';
 // animations
 import { motion } from 'framer-motion';
 import { fade, photoAnim, pageAnimation, lineAnim, slider, sliderContainer } from '../animation';
+import { useScroll } from '../components/useScroll';
+import ScrollTop from '../components/ScrollTop'
 
 
 const OurWork = () => {
+    const [element, controls] = useScroll();
+    const [element2, controls2] = useScroll();
     return (
-        <Work variants={pageAnimation} initial='hidden' animate='show'>
+        <Work variants={pageAnimation} initial='hidden' animate='show' exit='exit'>
             <motion.div variants={sliderContainer}>
                 <Frame1 variants={slider}></Frame1>
                 <Frame2 variants={slider}></Frame2>
@@ -19,7 +23,7 @@ const OurWork = () => {
                 <Frame4 variants={slider}></Frame4>
             </motion.div>
             <Movie>
-                <motion.h1 variants={fade}>The Athlete</motion.h1>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
                 <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to='/work/the-athlete'>
                     <Hide>
@@ -27,20 +31,21 @@ const OurWork = () => {
                     </Hide>
                 </Link>
             </Movie>
-            <Movie>
-                <h1>The Racer</h1>
-                <div className="line"></div>
+            <Movie ref={element} variants={fade} animate={controls} initial="hidden">
+                <h2>The Racer</h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to='/work/the-racer'>
                     <img src={theracer} alt="theracer" />
                 </Link>
             </Movie>
-            <Movie>
-                <h1>Good Times</h1>
-                <div className="line"></div>
+            <Movie ref={element2} variants={fade} animate={controls2} initial='hidden'>
+                <h2>Good Times</h2>
+                <motion.div variants={lineAnim} className="line"></motion.div>
                 <Link to='/work/good-times'>
                     <img src={goodtimes} alt="goodtimes" />
                 </Link>
             </Movie>
+            <ScrollTop />
         </Work>
     );
 };
@@ -53,7 +58,7 @@ const Work = styled(motion.div)`
         padding: 1rem 0rem;
     }
 `;
-const Movie = styled.div`
+const Movie = styled(motion.div)`
     padding-bottom: 10rem;
     .line{
         height: 0.5rem;
@@ -71,7 +76,7 @@ const Hide = styled.div`
     overflow: hidden;
 `;
 
-// fram animations
+// frame animations
 const Frame1 = styled(motion.div)`
     position: fixed;
     left: 0;
