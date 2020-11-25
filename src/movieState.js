4b74943df82e5ddfe3abc01fc10824f9db09ff0f
Dominic Varner <dommy@MLBs-Lappy.local>
2020-11-24 21:25:13 -0800

    changing up the themes

--- src/movieState.js
+++ src/movieState.js
@@ -2,9 +2,11 @@
 import athlete from "./img/athlete-small.png";
 import goodtimes from "./img/goodtimes-small.png";
 import theracer from "./img/theracer-small.png";
-import athlete2 from "./img/athlete2.png";
+import athlete2 from "./img/athlete2.jpg";
 import goodtimes2 from "./img/good-times2.jpg";
-import theracer2 from "./img/the-racer2.jpg";
+import musician2 from "./img/sunset-guitar.png";
+import wedding from "./img/wedding.png"
+import rapper from './img/rapper.png'
 
 export const MovieState = () => {
   return [
@@ -32,9 +34,9 @@ export const MovieState = () => {
       ],
     },
     {
-      title: "Good Times",
-      mainImg: goodtimes,
-      url: "/work/good-times",
+      title: "The Couple",
+      mainImg: wedding,
+      url: "/work/the-couple",
       secondaryImg: goodtimes2,
       awards: [
         {
@@ -55,10 +57,10 @@ export const MovieState = () => {
       ],
     },
     {
-      title: "The Racer",
-      mainImg: theracer,
-      url: "/work/the-racer",
-      secondaryImg: theracer2,
+      title: "The Musician",
+      mainImg: rapper,
+      url: "/work/the-musician",
+      secondaryImg: musician2,
       awards: [
         {
           title: "Truly A masterpiece",
